dbaad7f0fda5fe477027c120df2554de04d760fd
mayurpai <mayur5pai@gmail.com>
2025-01-21 23:42:18 +0530

    Update: [Desc: LabelConfig Changes]

---
+++ 2024/Apps/my-app/public/labelConfig.js
@@ -0,0 +1,30 @@
+window.labelConfig = {
+    global: {
+        appTitle: "My React Application",
+        confirmButton: "Confirm",
+        cancelButton: "Cancel",
+        loading: "Loading...",
+    },
+    home: {
+        welcome: "Welcome to Our App",
+        description: "Your one-stop solution for everything."
+    },
+    login: {
+        title: "Login",
+        usernamePlaceholder: "Enter your username",
+        passwordPlaceholder: "Enter your password",
+        submitButton: "Log In"
+    },
+    dashboard: {
+        greeting: "Hello, {name}!",
+        logout: "Log Out"
+    },
+    count: {
+        title: "Counters",
+    },
+    Button: {
+        logIn: "Login",
+        logOut: "Log Out",
+        addToCart: "Add To Cart"
+    }
+};
--- 2024/Apps/my-app/src/App.js
+++ 2024/Apps/my-app/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increment, reset } from './redux/counter/counterActions';
 import { countSelector } from './redux/counter/countSelector';
 
 const App = () => {
+    const labelConfig = window.labelConfig;
     const dispatch = useDispatch();
     const count = useSelector(countSelector)
     const [countPayload, setCountPayload] = useState()
     return (
         <>
-            <h1>Counter: {count}</h1>
+            <h1>{labelConfig.dashboard.greeting.replace("{name}", "Mayur")}</h1>
+            <h1>{labelConfig.count.title}: {count}</h1>
             <input type='number' onChange={(event) => setCountPayload(event.target.value)} />
             <button onClick={() => dispatch(increment(countPayload))}>Increment</button>
             <button onClick={() => dispatch(decrement(countPayload))}>Decrement</button>
